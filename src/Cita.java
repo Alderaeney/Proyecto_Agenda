d1ed613a0dd1bbcfcddec8edd57650eb2afbf22c
Alderaeney <alderaeney@protonmail.com>
2018-02-07 12:52:17 +0100

    Added some functions

--- src/Cita.java
+++ src/Cita.java
@@ -2,17 +2,24 @@ public class Cita {
 
     private String titulo;
     private String descripcion;
-    private int horaIni;
-    private int horaFin;
+    private int hora;
+    private int minutos;
 
-    public Cita(String titulo, String descripcion, int horaIni, int horaFin) {
+    public Cita(String titulo, String descripcion, int hora, int minutos) {
         this.titulo = titulo;
         this.descripcion = descripcion;
-        this.horaIni = horaIni;
-        this.horaFin = horaFin;
+        this.hora = hora;
+        this.minutos = minutos;
     }
 
+    public String leerCita(){
+        StringBuilder st = new StringBuilder();
 
+        st.append("").append(this.getHora()).append(":").append(this.getMinutos()).append("-");
+        st.append(this.getTitulo()).append("\n");
+        st.append(this.getDescripcion()).append("\n\n");
+        return st.toString();
+    }
 
     public String getTitulo() {
         return titulo;
@@ -30,19 +37,19 @@ public class Cita {
         this.descripcion = descripcion;
     }
 
-    public int getHoraIni() {
-        return horaIni;
+    public int getHora() {
+        return hora;
     }
 
-    public void setHoraIni(int horaIni) {
-        this.horaIni = horaIni;
+    public void setHora(int hora) {
+        this.hora = hora;
     }
 
-    public int getHoraFin() {
-        return horaFin;
+    public int getMinutos() {
+        return minutos;
     }
 
-    public void setHoraFin(int horaFin) {
-        this.horaFin = horaFin;
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
     }
 }
